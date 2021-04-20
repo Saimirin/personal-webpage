150903447ca41cb8807a55b91e2a1a74d8f3720d
meilun  difrancisco <ellen0816@hotmail.com>
2021-04-20 10:22:51 -0400

    created Navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
 
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar'
+
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h2>Meilun DiFrancisco</h2>
+      
     </div>
   );
 }
