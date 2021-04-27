57d0a63994b9efee5a6b8071358b21e49cf1968d
meilun  difrancisco <ellen0816@hotmail.com>
2021-04-27 00:30:56 -0400

    fixed the bug in Contacts comp

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,24 +1,17 @@
 import React, { useState } from "react";
 import emailjs from "emailjs-com";
-import {useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 const Contacts = () => {
-  const [successMessage,setSuccessMessage] = useState("");
-  const {register,handleSubmit,errors} = useForm();
+  const [successMessage, setSuccessMessage] = useState("");
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
   const serviceID = "service_ID";
   const templateID = "template_ID";
   const userID = "user_2seHjm5nPiEdSKAZKFrf5";
 
-  const sendEmail = (serviceID, templateID, variables, userID) => {
-    emailjs.send(serviceID, templateID, variables, userID)
-      .then(() => {
-        setSuccessMessage("Form sent successfully! I'll contact you as soon as possible.");
-      }).catch(err => console.error(`Something went wrong ${err}`));
-  }
-
-
   const onSubmit = (data, r) => {
+    console.log ("onSubmitting data...", data)
     sendEmail(
       serviceID,
       templateID,
@@ -27,20 +20,25 @@ const Contacts = () => {
         phone: data.phone,
         email: data.email,
         subject: data.subject,
-        message: data.message,
+        message: data.message
       },
       userID
     )
     r.target.reset();
   }
 
-
+  const sendEmail = (serviceID, templateID, variables, userID) => {
+    emailjs.send(serviceID, templateID, variables, userID)
+      .then(() => {
+        setSuccessMessage("Form sent successfully! I'll contact you as soon as possible.");
+      }).catch(err => console.error(`Something went wrong ${err}`));
+  }
 
   return (
     <div className="contacts">
       <div className="text-center">
         <h1>contact me</h1>
-        <p>Please fill out the form, and I'll contact you as soon as possible.</p>
+        <p>Please fill out the form and describe you project needs and I'll contact you as soon as possible.</p>
         <span className="success-message">{successMessage}</span>
       </div>
       <div className="container">
@@ -54,7 +52,15 @@ const Contacts = () => {
                   className="form-control"
                   placeholder="Name"
                   name="name"
-                 
+                  aria-invalid={errors.name ? "true" : "false"}
+                  {...register("name", {
+                    required: "Please enter your name",
+                    maxLength: {
+                      value: 20,
+                      message:
+                        "Please enter a name with fewer than 20 characters",
+                    },
+                    })}
                 />
                 <div className="line"></div>
               </div>
@@ -66,7 +72,10 @@ const Contacts = () => {
                   className="form-control"
                   placeholder="Phone Number"
                   name="phone"
-                  
+                  aria-invalid={errors.phone ? "true" : "false"}
+                  {...register("phone", {
+                    required: "Please add your phone number",
+                    })}
                 />
                 <div className="line"></div>
               </div>
@@ -78,12 +87,20 @@ const Contacts = () => {
                   className="form-control"
                   placeholder="Email"
                   name="email"
+                  aria-invalid={errors.email ? "true" : "false"}
+                  {...register("email", {
+                    required: "Please provide your email",
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message:
+                        "Invalid Email",
+                    },
+                    })}
                   
-            
                 />
                 <div className="line"></div>
               </div>
-            
+              
               {/* SUBJECT INPUT */}
               <div className="text-center">
                 <input
@@ -91,21 +108,24 @@ const Contacts = () => {
                   className="form-control"
                   placeholder="Subject"
                   name="subject"
-                  
+                  aria-invalid={errors.subject ? "true" : "false"}              
                 />
                 <div className="line"></div>
               </div>
-             
+              
             </div>
             <div className="col-md-6 col-xs-12">
+
               {/* MESSAGE */}
               <div className="text-center">
                 <textarea
                   type="text"
                   className="form-control"
-                  placeholder="Message"
+                  placeholder="Please describe shortly you project..."
                   name="message"
-                 
+                  {...register("message", {
+                    required: "Please leave a short message...",
+                    })}
                 ></textarea>
                 <div className="line"></div>
               </div>
