c3e03414c47d757f71a6f9cc5f0f79921ac61d59
meilun  difrancisco <ellen0816@hotmail.com>
2021-05-13 00:56:10 -0400

    changed the header image

--- src/App.js
+++ src/App.js
@@ -12,12 +12,66 @@ import Recommendation from './components/Recommendation';
 import Footer from './components/Footer';
 import Skill from './components/Skill';
 import Artical from './components/Artical';
-import Particles from "react-tsparticles";
+//import Particles from "react-tsparticles";
+import Particles from "react-particles-js";
 
 
 function App() {
   return (
     <div className="App">
+      <Particles
+    params={{
+	    "particles": {
+	        "number": {
+	            "value": 90,
+	            "density": {
+	                "enable": false
+	            }
+	        },
+          "color":"#FBE8A6",
+	        "size": {
+	            "value": 30,
+	            "random": true,
+	            "anim": {
+	                "speed": 4,
+	                "size_min": 0.3
+	            }
+	        },
+	        "line_linked": {
+	            "enable": false
+	        },
+	        "move": {
+	            "random": true,
+	            "speed": 1,
+	            "direction": "random",
+	            "out_mode": "out"
+	        }
+	    },
+	    "interactivity": {
+	        "events": {
+	            "onhover": {
+	                "enable": true,
+	                "mode": "bubble"
+	            },
+	            "onclick": {
+	                "enable": true,
+	                "mode": "repulse"
+	            }
+	        },
+	        "modes": {
+	            "bubble": {
+	                "distance": 250,
+	                "duration": 2,
+	                "size": 0,
+	                "opacity": 0
+	            },
+	            "repulse": {
+	                "distance": 400,
+	                "duration": 4
+	            }
+	        }
+	    }
+	}} />
       
       <Navbar />
       <Header />
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,78 +6,11 @@ import Particles from "react-tsparticles";
 const Header = () => {
     return (
         <div className="header-wraper">
-            {/* <Particles
-      options={{
-        backgroundMode: {
-          enable: true,
-          zIndex: 0
-        },
-       
-        fpsLimit: 60,
-        interactivity: {
-          detectsOn: "canvas",
-          events: {
-            onClick: { enable: true, mode: "repulse" },
-            onHover: {
-              enable: true,
-              mode: "bubble",
-              parallax: { enable: false, force: 2, smooth: 10 }
-            },
-            resize: true
-          },
-          modes: {
-            bubble: {
-              distance: 400,
-              duration: 0.3,
-              opacity: 1,
-              size: 4,
-              speed: 3
-            },
-            grab: { distance: 400, line_linked: { opacity: 0.5 } },
-            push: { particles_nb: 4 },
-            remove: { particles_nb: 2 },
-            repulse: { distance: 200, duration: 0.4 }
-          }
-        },
-        particles: {
-          color: { value: "#fff" },
-          links: {
-            color: "#ffffff",
-            distance: 500,
-            enable: false,
-            opacity: 0.4,
-            width: 2
-          },
-          move: {
-            attract: { enable: false, rotateX: 600, rotateY: 1200 },
-            direction: "bottom",
-            enable: true,
-            outMode: "out",
-            random: false,
-            size: true,
-            speed: 4,
-            straight: false
-          },
-          number: { density: { enable: true, area: 800 }, value: 400 },
-          opacity: {
-            random: true,
-            value: 0.5
-          },
-          shape: {
-            type: "circle"
-          },
-          size: {
-            random: true,
-            value: 10
-          }
-        },
-        detectRetina: true
-      }}
-    /> */}
+           
             <div className="main-info">
                 <canvas></canvas>
                 <h1>Hi,I am Meilun! </h1>
-                <h1> I'm a...</h1>
+                <h2> I'm a...</h2>
                 <div> <Typed 
                     className="typed-text"
                     strings={["Full Stack Software Developer", "Backend Engineer", "Data Scientist", "Machine Learning engineer"]}
@@ -88,7 +21,7 @@ const Header = () => {
                     smartBackspace
                 />
                 </div>
-                {/* <h5> (This web page is still under processing...)</h5> */}
+                <h5> (This web page is still under processing...)</h5>
                 <Link smooth={true} to="contact" className="btn-main-offer">contact me</Link>
 
                 
